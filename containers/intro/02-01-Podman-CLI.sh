bf7d105cca64a80e96cd95ce68c03b13fce33544
Valentin Rothberg <vrothberg@redhat.com>
2022-09-06 10:11:55 +0200

    add containers/intro/02-01-Podman-CLI.sh
    A session on Podman's command-line interface, Docker compatibility
    and the architectural differences between Docker and Podman.
    
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

---
+++ containers/intro/02-01-Podman-CLI.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+source helpers.bash
+
+setup
+
+# Require evince to display PDF files.
+require_tool evince
+
+
+# Show the `container --help` and sub-commands of Docker and Podman.
+# Mention that
+#  * Docker made containers popular and successful
+#  * Many users (including the Podman developers) have already been accustomed to the Docker CLI
+#  * Podman is CLI compatible to Docker but added a number of new commands
+#  * The architecture of the two tools is very different
+run_command sudo docker container --help
+clear
+
+run_command podman container --help
+clear
+
+# Show some more podman commands and explain the command structure.
+# Sub-commands are usually structured by "objects" such as `podman {image,container,pod} $command`.
+run_command podman image --help
+clear
+
+# Some commands have top-level aliases.  Those are usually the "hot" ones and used more frequently than others.
+run_podman podman pull --help
+clear
+
+# Show a PDF illustrating the architectural differences between Docker and Podman
+run_command evince ./containers/intro/data/02-01-architecture-comparision.pdf
