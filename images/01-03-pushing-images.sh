6a034cbed03d3eae9da565296f5370881dd50164
Valentin Rothberg <vrothberg@redhat.com>
2022-10-24 13:28:05 +0200

    add images/01-04-remote-inspection.sh
    Add a tutorial on remote inspecting images with Skopeo.
    
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

--- images/01-03-pushing-images.sh
+++ images/01-03-pushing-images.sh
@@ -13,24 +13,29 @@ setup
 
 QUAY_IMAGE=quay.io/$REGISTRY_USER/tutorial:image
 
+# Image push will fail -> we need to login.
 run_podman push $IMAGE $QUAY_IMAGE
 clear
 
+# Login and repush.
 run_podman login -u $REGISTRY_USER quay.io
 
 run_podman push $IMAGE $QUAY_IMAGE
 clear
 
+# Show that push also works with a single argument.
 run_podman tag $IMAGE $QUAY_IMAGE
 
 run_podman push $QUAY_IMAGE
 clear
 
+# Push to a local directory to highlight that there are several "transports".
 tmp=$(mktemp -d -p .)
 run_podman push $QUAY_IMAGE dir:$tmp
 
 run_command tree dir:$tmp
 clear
 
+# Open the transports man page and browse through it to give a feeling for what's supported.
 run_command man containers-transports
 rm -rf $tmp
---
+++ images/01-04-remote-inspection.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+source helpers.bash
+
+# Skopeo is like a Swiss army knife for container images. It was built for
+# inspecting container images on the registry without having to pull it down.
+# Since then, it grew into a powerful tool to manage container images.
+
+# Show the main help message to give an impression of what Skopeo can do.
+run_command skopeo --help
+clear
+
+# Show a simple remote inspect
+cmd="skopeo inspect docker://$SOURCE_IMAGE"
+prompt "\$ $cmd"
+$cmd | less
+clear
+
+# Show the --config flag
+cmd="skopeo inspect --config docker://$SOURCE_IMAGE"
+prompt "\$ $cmd"
+$cmd | jq . | less
+clear
+
+# Show the --raw flag
+cmd="skopeo inspect --raw docker://$SOURCE_IMAGE"
+prompt "\$ $cmd"
+$cmd | jq . | less
+clear
