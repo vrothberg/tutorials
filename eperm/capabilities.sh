ba5f3c6eb08d556700cee0a752caf56542d7451e
Valentin Rothberg <vrothberg@redhat.com>
2022-11-08 12:30:49 +0100

    eperm updates
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

--- eperm/capabilities.sh
+++ eperm/capabilities.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env sh
 
 source helpers.bash
+
+run_command man capabilities
+clear
+
+dockerfile=$(mktemp --suffix ".Dockerfile")
+cat >$dockerfile <<EOF
+FROM fedora:37
+RUN  mknod /dev/mynull c 1 3
+EOF
+
+run_command cat $dockerfile
+
+run_podman_root build --no-cache -f $dockerfile
+clear
+
+run_podman_root build --no-cache -f $dockerfile --cap-add=all
+clear
+
+run_podman_root build --no-cache -f $dockerfile --cap-add=MKNOD
+clear
