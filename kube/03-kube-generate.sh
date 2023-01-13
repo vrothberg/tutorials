80866b233657dedd21de2fc0609b5143bce6f6ca
Valentin Rothberg <vrothberg@redhat.com>
2023-01-13 11:10:26 +0100

    kube/03-kube-generate.sh
    Add a tutorial on generate K8s YAML files with Podman.
    It's still work in progress as I found some ugly warnings emitted by
    Podman that I intend on fixing before.
    
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

---
+++ kube/03-kube-generate.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env sh
+
+source helpers.bash
+
+run_podman_no_prompt rm -af
+run_podman_no_prompt docker.io/mariadb docker.io/wordpress
+clear
+
+# This tutorial is meant to show how users can move their local containers and
+# pods to a K8s YAML and thereby to Kubernetes and OpenShift.
+
+run_podman pod create --name=tutorial-pod -p 8080:80
+clear
+
+run_podman run -d --pod=tutorial-pod \
+  -e MYSQL_ROOT_PASSWORD="tutorial" \
+  -e MYSQL_DATABASE="tutorial-db" \
+  -e MYSQL_USER="tutorial-user" \
+  -e MYSQL_PASSWORD="tutorial-pw" \
+  --name=tutorial-db docker.io/mariadb
+clear
+
+run_podman run -d --pod=tutorial-pod \
+  -e WORDPRESS_DB_NAME="tutorial-db" \
+  -e WORDPRESS_DB_USER="tutorial-user" \
+  -e WORDPRESS_DB_PASSWORD="tutorial-pw" \
+  -e WORDPRESS_DB_HOST="127.0.0.1" \
+  --name=tutorial-wp docker.io/wordpress
+clear
+
+run_podman pod ps
+run_podman ps
+clear
+
+kubefile=$(mktemp --suffix ".yaml")
+run_podman kube generate tutorial-pod > $kubefile
+run_command cat $kubefile
+clear
+
+run_podman kube play $kubefile
+clear
+
+run_podman pod ps
+run_podman ps
+clear
+
+run_command firefox --new-window http://localhost:8080
+clear
