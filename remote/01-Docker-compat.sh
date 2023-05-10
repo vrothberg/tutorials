179f6f8134db79ac880c235e8730585282922cf3
Valentin Rothberg <vrothberg@redhat.com>
2023-05-10 10:55:54 +0200

    remote/01-Docker-compat.sh: add docker-compose example
    Tutorial should be ready for recording now.
    
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

--- remote/01-Docker-compat.sh
+++ remote/01-Docker-compat.sh
@@ -25,6 +25,27 @@ clear
 docker="DOCKER_HOST=unix://$XDG_RUNTIME_DIR/podman/podman.sock docker"
 
 run_command systemctl --user start podman.socket
+clear
+
 run_command eval "$docker" pull alpine
 run_command eval $docker images
 run_command eval $docker run --rm alpine echo \"I am running a Podman container with the Docker client\"
+clear
+
+# Now, let's look at the veeeery common use case of using docker-compose.
+# Does it work with Podman?  Yes!  Make sure to mention that the podman-compose
+# project is a community-only project and was created at a time where Podman
+# did not have Docker-compatible REST API yet.  There is choice but no
+# enterprise support for either yet.
+
+compose="./remote/data/compose.yaml"
+run_command less $compose
+clear
+
+run_command docker-compose --host=unix://$XDG_RUNTIME_DIR/podman/podman.sock -f $compose up -d
+run_podman ps
+clear
+
+run_command firefox --new-window http://localhost:8080
+run_command docker-compose --host=unix://$XDG_RUNTIME_DIR/podman/podman.sock -f $compose down
+run_podman ps
