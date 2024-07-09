6ca13ebc6b568de42a49475fa758a5feb1d6ce97
Valentin Rothberg <vrothberg@redhat.com>
2024-07-09 14:25:12 +0200

    add a bootc demo
    More to come, I am sure.
    
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

---
+++ bootc/demo-july-15-24/demo.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env sh
+
+bold=$(tput bold)      
+reset=$(tput sgr0)     
+color="$(tput setaf 2)"
+
+if [ `id -u` -ne 0 ]; then
+	echo "Please run as root"
+	exit 1
+fi
+
+function prompt() {                    
+    read -p "${bold}${color}$1${reset}"
+}                                      
+                                       
+function echo_bold() {                 
+    echo "${bold}${color}$1${reset}"   
+}                                      
+                                       
+function run_command() {               
+        prompt "\$ $*"                 
+        command $@                     
+        read -p ""                     
+}                                      
+                                       
+function run_command_no_prompt() {     
+        echo_bold "\$ $*"              
+        $@                             
+        echo ""                        
+}                                      
+
+prompt "Example scenario RHEL SST QEs will face going forward. Best practices to test on Image Mode?"
+echo ""
+
+prompt "Task: make sure the integration tests of github.com/vrothberg/vgrep pass in Image Mode."
+echo ""
+
+prompt "After a bit of fiddling, I ended up with the following Dockerfile:"
+
+
+run_command nvim Dockerfile
+clear
+
+run_command podman build -t vgrep:bootc .
+clear
+
+
+prompt "The user stories explicitly mentioned a local dev-test cycle with Podman and the OCI container before transitioning to an Image Mode host. So let's run the tests in a local Podman container:"
+run_command podman run -d --name=vgrep --replace vgrep:bootc
+run_command podman exec -it vgrep bash
+clear
+
+prompt "The next steps have already been demoed:"
+prompt " * Use bootc-image-builder to convert and boot the disk image."
+prompt " * User podman-bootc to automate the conversion and booting."
+clear
+
+prompt "Lessons learned ..."
+prompt " 1) Bootc images are OCI images with specific attributes users need to be familiar with"
+prompt " 2) The docs contain all information but they are scattered.  We need on-boarding docs and provide best practices."
+prompt " 3) After RHEL 9.5 I want more attention for the developer experience.
