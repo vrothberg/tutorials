05b938b9e3d33e1799e49f92260ca1183eebdf9c
Valentin Rothberg <vrothberg@redhat.com>
2024-07-10 09:59:00 +0200

    bootc: more polishing
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

--- bootc/demo-july-15-24/demo.sh
+++ bootc/demo-july-15-24/demo.sh
@@ -47,17 +47,28 @@ clear
 
 
 prompt "The user stories explicitly mentioned a local dev-test cycle with Podman and the OCI container before transitioning to an Image Mode host. So let's run the tests in a local Podman container:"
-run_command podman run -d --name=vgrep --replace vgrep:bootc
+echo""
+run_command podman run --detach --name=vgrep --replace vgrep:bootc
+
+echo""
+prompt "Running the container in --detached mode allows for systemd to be PID1 and initialize the bootc container as intended. We can now exec' into the container to get some work done:"
+echo ""
 run_command podman exec -it vgrep bash
 clear
 
-prompt "The next steps have already been demoed:"
+prompt "The next steps in the developer workflow have already been demoed:"
+echo ""
 prompt " * Use bootc-image-builder to convert and boot the disk image."
+echo ""
 prompt " * Use podman-bootc to automate the conversion and booting."
 clear
 
 prompt "Lessons learned ..."
-prompt " 1) Bootc images are OCI images with specific attributes users need to be familiar with."
+echo ""
+prompt " 1) Bootc images are OCI images with specific attributes users need to be familiar with. They do behave differently!"
+echo ""
 prompt " 2) When working locally, make sure to start the bootc container with systemd and then podman-exec into it. This makes sure the systemd services have fired."
+echo ""
 prompt " 3) The docs contain all information but they are scattered.  We need on-boarding docs and provide best practices."
-prompt " 4) After RHEL 9.5 I want more attention for the developer experience."
+echo ""
+prompt " 4) After RHEL 9.5 I want to pay more attention to the developer experience and enablement."
