f1a6b5fadd7c32135ef05d29b0e5d1e14967aa55
Valentin Rothberg <vrothberg@redhat.com>
2024-07-17 11:53:52 +0200

    add a demo to show the build-boot-upgrade cycle
    Pretty smooth experience but note that it requires
    a 2nd terminal with some user interaction using
    podman-bootc or whatever floats your boat.
    
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

---
+++ bootc/build-and-boot/demo.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+source helpers.bash
+
+image="quay.io/vrothberg/build-and-boot:bootc"
+
+clear
+prompt "Example scenario: Build and boot a bootable container and ship an update."
+echo ""
+
+prompt "A simple Dockerfile based on the fedora-bootc:"
+
+run_command nvim bootc/build-and-boot/Dockerfile
+clear
+
+run_command podman build -t $image bootc/build-and-boot
+clear
+
+run_command podman images $image
+clear
+
+prompt "Let's use podman-bootc to run it in a VM!"
+clear
+
+prompt "Let's pretend we need to push an urgent update!"
+
+run_command nvim bootc/build-and-boot/Dockerfile.fix
+clear
+
+run_command podman build -t $image -f bootc/build-and-boot/Dockerfile.fix
+clear
+
+prompt "Now we can push the update to the registry!"
+run_command podman push $image
