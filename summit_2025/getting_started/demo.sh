fac4cf0449be7b26d14e3b01ea15abb225e9637a
Valentin Rothberg <vrothberg@redhat.com>
2025-04-18 09:43:02 +0200

    add summit 2025 demo: getting started lightning talk
    Signed-off-by: Valentin Rothberg <vrothberg@redhat.com>

---
+++ summit_2025/getting_started/demo.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+
+source helpers.bash
+
+image="quay.io/vrothberg/bootc:demo"
+dir=$(dirname ${BASH_SOURCE[0]})
+
+clear
+prompt "Hands on: bootable containers."
+
+clear
+prompt "Example scenario: Build and boot a bootable container and ship an update."
+echo ""
+
+prompt "A simple Containerfile based on centos-bootc:stream10"
+
+run_command nvim $dir/Containerfile
+clear
+
+run_command podman build -t $image -f $dir/Containerfile
+clear
+
+run_command podman images $image
+clear
+
+prompt "Let's use podman-bootc to run it in a VM!"
+clear
+
+prompt "Let's fix the bug and update our VM!"
+
+run_command nvim $dir/Containerfile.fix
+clear
+
+run_command podman build -t $image -f $dir/Containerfile.fix
+clear
+
+prompt "Now we can push the update to the registry!"
+run_command podman push $image
+clear
+
+prompt "Let's update the VM!"
+clear
+
+prompt "And finally, let's rollback!"
+clear
